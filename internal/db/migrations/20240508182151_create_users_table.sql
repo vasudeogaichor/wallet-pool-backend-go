2ef75ff835be0930ae56fc480b57b4cf5f0d6dda
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-05-09 00:02:13 +0530

    started using goose for db migrations

--- internal/db/migrations/20240508182151_create_users_table.sql
+++ internal/db/migrations/20240508182151_create_users_table.sql
@@ -1,3 +1,6 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR NOT NULL,
@@ -7,3 +10,10 @@ CREATE TABLE users (
     mobile VARCHAR UNIQUE,
     password VARCHAR NOT NULL
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE users;
+-- +goose StatementEnd
