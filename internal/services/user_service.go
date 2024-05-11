ae4c6b0812e88e7573e901840da9b84aab715018
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-05-11 23:36:20 +0530

    added dummy service

---
+++ internal/services/user_service.go
@@ -0,0 +1,11 @@
+package services
+
+import (
+	"github.com/vasudeogaichor/wallet-pool-backend/internal/db/models"
+)
+
+type UserService struct{}
+
+func (s *UserService) CreateUser(user *models.User) error {
+	return nil
+}
